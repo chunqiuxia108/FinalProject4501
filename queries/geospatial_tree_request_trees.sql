4e473ea0eb0d58bc9ca3931878e4ff1aa21dd1b3
chunqiuxia108 <147873073+chunqiuxia108@users.noreply.github.com>
2023-12-03 20:12:54 -0500

    Update geospatial_tree_request_trees.sql

--- queries/geospatial_tree_request_trees.sql
+++ queries/geospatial_tree_request_trees.sql
@@ -9,28 +9,3 @@
     WHERE created_date BETWEEN '2018-10-01' AND '2023-09-30'
     AND complaint_type = 'New Tree Request';
     
-
-
-    SELECT latitude, longitude
-    FROM trees;
-    
-
-
-    SELECT latitude, longitude
-    FROM three_11s
-    WHERE created_date BETWEEN '2018-10-01' AND '2023-09-30'
-    AND complaint_type = 'New Tree Request';
-    
-
-
-    SELECT latitude, longitude
-    FROM trees;
-    
-
-
-    SELECT latitude, longitude
-    FROM three_11s
-    WHERE created_date BETWEEN '2018-10-01' AND '2023-09-30'
-    AND complaint_type = 'New Tree Request';
-    
-
