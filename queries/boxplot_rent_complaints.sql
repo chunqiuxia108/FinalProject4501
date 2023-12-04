11aa4d45320d83aa8d5e65ab8d32b2fe21094261
allenyang763 <147768895+allenyang763@users.noreply.github.com>
2023-12-03 21:06:48 -0500

    Update boxplot_rent_complaints.sql
    the code run twice when testing

--- queries/boxplot_rent_complaints.sql
+++ queries/boxplot_rent_complaints.sql
@@ -8,17 +8,5 @@
     FROM three_11s
     WHERE created_date >= '2022-10-01' AND created_date <= '2023-09-30'
     GROUP BY zipcode;
-    
 
-
-    SELECT zipcode, date_2023_09_30 AS rent
-    FROM zillows;
-    
-
-
-    SELECT zipcode, COUNT(*) AS num_complaints
-    FROM three_11s
-    WHERE created_date >= '2022-10-01' AND created_date <= '2023-09-30'
-    GROUP BY zipcode;
-    
 
